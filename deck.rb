38184f11452307896dad71a1d14b0973cc3f29fd
sunchichi0505 <sunchichi0505@gmail.com>
2021-06-22 05:34:55 +0900

    basic_functions_complete

---
+++ deck.rb
@@ -0,0 +1,28 @@
+class Deck
+    #カードデッキを作成し、シャッフルまで行う
+    def initailize
+        #デッキの配列を生産
+        @cards = []
+
+        mk = ["スペード", "ハート", "ダイア", "クラブ"]
+        num = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+    
+
+        mk.each do |mark|
+            num.each do |number|
+                #markとnumberの組み合わせを一つずつcardクラスに渡し、それぞれのcardオブジェクトを生成する
+                card = Card.new(mark, number)
+                #生成したcardを配列に格納していく
+                @card << card
+            end
+        end
+
+        #デッキをシャッフルする
+        @cards.shuffle!
+    end
+
+    def draw
+        #配列の最初の要素を抜き出す。最後から抜き出す場合はpop
+        @card.shift
+    end
+end
